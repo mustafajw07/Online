470b6568dcc20b53bef2d277a4b478861e783c09
unknown <mustafajw07@gmail.com>
2022-10-13 10:59:28 +0530

    Done

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,15 +1,16 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
-import { ContactUsPageComponent } from './components/contact-us-page/contact-us-page.component';
-import { ErrorPageComponent } from './components/error-page/error-page.component';
-import { HomePageComponent } from './components/home-page/home-page.component';
-import { OurProductsComponent } from './components/our-products/our-products.component';
+import { ContactUsPageComponent } from './pages/contact-us-page/contact-us-page.component';
+import { ErrorPageComponent } from './pages/error-page/error-page.component';
+import { HomePageComponent } from './pages/home-page/home-page.component';
+import { OurProductsComponent } from './pages/our-products/our-products.component';
+
 
 
 const routes: Routes = [
   {path : '' , component : HomePageComponent},
-  {path : 'our-products' , component : OurProductsComponent},
-  {path : 'contact-us' , component : ContactUsPageComponent},
+  {path : 'products' , component : OurProductsComponent},
+  {path : 'contact' , component : ContactUsPageComponent},
   {path : '**' , component : ErrorPageComponent},
 ];
 
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,24 +3,26 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { ContactUsPageComponent } from './pages/contact-us-page/contact-us-page.component';
+import { ErrorPageComponent } from './pages/error-page/error-page.component';
+import { HomePageComponent } from './pages/home-page/home-page.component';
 
-import { ContactUsPageComponent } from './components/contact-us-page/contact-us-page.component';
-import { ErrorPageComponent } from './components/error-page/error-page.component';
-import { HomePageComponent } from './components/home-page/home-page.component';
-import { OurProductsComponent } from './components/our-products/our-products.component';
-import { FooterComponent } from './shared/footer/footer.component';
-import { HeaderComponent } from './shared/header/header.component';
+
+import { FooterComponent } from './Partials/footer/footer.component';
+import { HeaderComponent } from './Partials/header/header.component';
+import { OurProductsComponent } from './pages/our-products/our-products.component';
+
 
 
 @NgModule({
   declarations: [
     AppComponent,
-    ErrorPageComponent,
-    ContactUsPageComponent,
-    HomePageComponent,
-    OurProductsComponent,
     HeaderComponent,
-    FooterComponent
+    FooterComponent,
+    HomePageComponent,
+    ContactUsPageComponent,
+    ErrorPageComponent,
+    OurProductsComponent
   ],
   imports: [
     BrowserModule,
