32068f60f5aba0d514f7433d0311d9027293b938
unknown <mustafajw07@gmail.com>
2022-10-13 16:51:34 +0530

    First Commit

--- src/app/shared/material.module.ts
+++ src/app/shared/material.module.ts
@@ -1,12 +1,35 @@
 import { NgModule } from '@angular/core';
-import { CommonModule } from '@angular/common';
+import {MatButtonModule} from '@angular/material/button';
+import {MatFormFieldModule} from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input';
+import {MatDatepickerModule} from '@angular/material/datepicker';
+import {MatNativeDateModule} from '@angular/material/core';
+import {MatCardModule} from '@angular/material/card';
+import {MatListModule} from '@angular/material/list';
+import {DragDropModule} from '@angular/cdk/drag-drop';
+import {MatSnackBarModule} from '@angular/material/snack-bar';
+import {MatToolbarModule} from '@angular/material/toolbar';
+import {MatIconModule} from '@angular/material/icon';
+import {MatDialogModule} from '@angular/material/dialog';
 
+const Material:any = [
+  MatButtonModule,
+  MatFormFieldModule,
+  MatInputModule,
+  MatDatepickerModule,
+  MatNativeDateModule,
+  MatCardModule,
+  MatListModule,
+  DragDropModule,
+  MatSnackBarModule,
+  MatToolbarModule,
+  MatIconModule,
+  MatDialogModule
+]
 
 
 @NgModule({
-  declarations: [],
-  imports: [
-    CommonModule
-  ]
+  imports: [Material],
+  exports: [Material]
 })
 export class MaterialModule { }
