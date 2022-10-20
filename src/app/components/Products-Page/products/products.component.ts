abff365b66858e949226d357df36eb5d79183e3a
mustafa jawadwala <mustafajw07@gmail.com>
2022-10-20 17:30:38 +0530

    Single-Product-Page

--- src/app/components/Products-Page/products/products.component.ts
+++ src/app/components/Products-Page/products/products.component.ts
@@ -1,4 +1,5 @@
 import { Component, Input, OnChanges } from '@angular/core';
+import { ApiService } from 'src/app/services/api.service';
 
 @Component({
   selector: 'app-products',
@@ -7,12 +8,38 @@ import { Component, Input, OnChanges } from '@angular/core';
 })
 export class ProductsComponent implements OnChanges {
   @Input() id! : number;
-  constructor() { }
+  productsList:any;
+  Title = 'All';
+  pageNumber: number = 1;
+  constructor(private api: ApiService) { }
 
   ngOnChanges(changes: any) {
     this.id = changes.id.currentValue;
     if(this.id == undefined || null){
       this.id = 0;
+    }
+    if(this.id == 0){
+      this.api.getAllAppleProducts().subscribe((res) => this.productsList = res);
+    }
+    if(this.id == 1){
+      this.api.getAllAppleProducts().subscribe((res) => this.productsList = res);
+      this.Title = "Apple";
+      this.pageNumber = 1;
+    }
+    if(this.id == 2){
+      this.api.getAllDrDreProducts().subscribe((res) => this.productsList = res);
+      this.Title = "Dr Dre";
+      this.pageNumber = 1;
+    }
+    if(this.id == 3){
+      this.api.getAllRollingSquareProducts().subscribe((res) => this.productsList = res);
+      this.Title = "Rolling Square";
+      this.pageNumber = 1;
+    }
+    if(this.id == 4){
+      this.api.getAllSatechiProducts().subscribe((res) => this.productsList = res);
+      this.Title = "Satechi";
+      this.pageNumber = 1;
     }
   }
 
---
+++ src/app/components/Single-Product-Page/recommend-products/recommend-products.component.ts
@@ -0,0 +1,49 @@
+import { Component, Input, OnInit } from '@angular/core';
+import { ApiService } from 'src/app/services/api.service';
+
+@Component({
+  selector: 'app-recommend-products',
+  templateUrl: './recommend-products.component.html',
+  styleUrls: ['./recommend-products.component.css']
+})
+export class RecommendProductsComponent implements OnInit {
+  @Input() cid! : number;
+  productList:any = [];
+  constructor(private api: ApiService) { }
+
+  ngOnInit(): void {
+    if(this.cid == 1){
+      this.api.getAllAppleProducts().subscribe((res:any) => {
+        for (let index = 0; index < 4; index++) {
+          let x = Math.floor((Math.random() * res.length-1) + 1);
+          this.productList.push(res[x])
+        }
+      });
+    }
+    if(this.cid == 2){
+      this.api.getAllDrDreProducts().subscribe((res:any) => {
+        for (let index = 0; index < 4; index++) {
+          let x = Math.floor((Math.random() * res.length-1) + 1);
+          this.productList.push(res[x])
+        }
+      });
+    }
+    if(this.cid == 3){
+      this.api.getAllRollingSquareProducts().subscribe((res:any) => {
+        for (let index = 0; index < 4; index++) {
+          let x = Math.floor((Math.random() * res.length-1) + 1);
+          this.productList.push(res[x])
+        }
+      });
+    }
+    if(this.cid == 4){
+      this.api.getAllSatechiProducts().subscribe((res:any) => {
+        for (let index = 0; index < 4; index++) {
+          let x = Math.floor((Math.random() * res.length-1) + 1);
+          this.productList.push(res[x])
+        }
+      });
+    }
+  }
+
+}
