c0e06caa29e2a25cfeb541f43fa7392da5d41a99
mustafa jawadwala <mustafajw07@gmail.com>
2023-01-06 19:16:24 +0530

    contact form added

--- src/app/components/whatsappbtn/whatsappbtn.component.ts
+++ src/app/components/whatsappbtn/whatsappbtn.component.ts
@@ -1,15 +1,11 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-whatsappbtn',
   templateUrl: './whatsappbtn.component.html',
   styleUrls: ['./whatsappbtn.component.css']
 })
-export class WhatsappbtnComponent implements OnInit {
+export class WhatsappbtnComponent{
 
   constructor() { }
-
-  ngOnInit(): void {
-  }
-
 }
